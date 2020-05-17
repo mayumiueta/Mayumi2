1c2edf09e1b7c2af0f8a15dea457e2e3a8860a84
mayumiueta <mueta@koto.kpu-m.ac.jp>
2020-05-17 20:46:33 +0900

    Initial commit

---
+++ app/src/main/java/com/example/mayumi2/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.mayumi2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // 各IDに,setonClicklisteiner
+        //snack bar  で右です。左です。
+    }
+
+  //  class PieChart(context: Context, attrs: AttributeSet) : View(context, attrs) //
+
+}
